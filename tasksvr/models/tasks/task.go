ee7eb5217e5c14ed03490e03612afc702b174e40
Your Name <you@example.com>
2017-04-17 15:19:47 -0700

    4/17 in class

--- tasksvr/models/tasks/task.go
+++ tasksvr/models/tasks/task.go
@@ -1,23 +1,43 @@
 package tasks
 
+import (
+	"fmt"
+	"time"
+)
+
 //NewTask represents a new task posted to the server
 type NewTask struct {
+	Title string   `json:"title"`
+	Tags  []string `json:"tags"`
 	//TODO: fill out fields
 }
 
 //Task represents a task stored in the database
 type Task struct {
-	//TODO: fill out fields
+	ID         interface{} `json:"id" bson:"_id"`
+	Title      string      `json:"title"`
+	Tags       []string    `json:"tags"`
+	CreatedAt  time.Time   `json:"createdAt"`
+	ModifiedAt time.Time   `json:"modifiedAt"`
+	Complete   bool        `json:"complete"`
 }
 
 //Validate will validate the NewTask
 func (nt *NewTask) Validate() error {
-	//TODO: implement
+	//title field must be non zero length
+	if len(nt.Title) == 0 {
+		return fmt.Errorf("title must be something")
+	}
 	return nil
 }
 
 //ToTask converts a NewTask to a Task
 func (nt *NewTask) ToTask() *Task {
-	//TODO: implement
-	return nil
+	t := &Task{
+		Title:      nt.Title,
+		Tags:       nt.Tags,
+		CreatedAt:  time.Now(),
+		ModifiedAt: time.Now(),
+	}
+	return t
 }
