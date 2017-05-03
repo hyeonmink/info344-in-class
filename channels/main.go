e27c032612887cdab1115ef960b4f830646242af
Your Name <you@example.com>
2017-05-03 15:15:27 -0700

    5/3

--- channels/main.go
+++ channels/main.go
@@ -1,10 +1,17 @@
 package main
 
+import "math/rand"
+import "time"
+import "fmt"
+
 //someLongFunc is a function that might
 //take a while to complete, so we want
 //to run it on its own go routine
-func someLongFunc() {
-
+func someLongFunc(ch chan int) {
+	r := rand.Intn(2000)
+	d := time.Duration(r)
+	time.Sleep(time.Millisecond * d)
+	ch <- r
 }
 
 func main() {
@@ -14,4 +21,18 @@ func main() {
 	//passing the channel so that
 	//someLongFunc() can communicate
 	//its results
+	start := time.Now()
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("starting long-running func...")
+	n := 10
+	ch := make(chan int, n)
+	for i := 0; i < n; i++ {
+		go someLongFunc(ch)
+	}
+
+	for i := 0; i < n; i++ {
+		result := <-ch
+		fmt.Printf("result was %d\n", result)
+	}
+	fmt.Printf("took %v\n", time.Since(start))
 }
--- concur/main.go
+++ concur/main.go
@@ -1,38 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
+	"strings"
 	"time"
 )
 
 const usage = `
 usage:
-	concur <data-dir-path>
+	concur <data-dir-path> <search-string>
 `
 
-func processFile(filePath string, ch chan int) {
+func processFile(filePath string, q string, ch chan []string) {
 	//TODO: open the file, scan each line,
 	//do something with the word, and write
 	//the results to the channel
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	matches := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		word := scanner.Text()
+		if strings.Contains(word, q) {
+			matches = append(matches, word)
+		}
+	}
+	f.Close()
+	ch <- matches
 }
 
-func processDir(dirPath string) {
+func processDir(dirPath string, q string) {
 	//TODO: iterate over the files in the directory
 	//and process each, first in a serial manner,
 	//and then in a concurrent manner
+	fileinfos, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ch := make(chan []string, len(fileinfos))
+	for _, fi := range fileinfos {
+		// go processFile(path.Join(dirPath, fi.Name()), ch)
+		processFile(path.Join(dirPath, fi.Name()), q, ch)
+	}
+
+	totalMathces := []string{}
+	for i := 0; i < len(fileinfos); i++ {
+		matches := <-ch
+		totalMathces = append(totalMathces, matches...)
+	}
+	fmt.Println(totalMathces)
+
 }
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
 
 	dir := os.Args[1]
-
+	q := os.Args[2]
 	fmt.Printf("processing directory %s...\n", dir)
 	start := time.Now()
-	processDir(dir)
+	processDir(dir, q)
 	fmt.Printf("completed in %v\n", time.Since(start))
 }
